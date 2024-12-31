b5d493f00e6d0bdcd3244d94cea34eeb6900b110
Maddison Hellstrom <maddy@na.ai>
2024-12-30 23:53:01 -0800

    chore(docs): update gen.sh

--- doc/gen.sh
+++ doc/gen.sh
@@ -10,8 +10,8 @@ reporoot="$(realpath "$basedir/..")"
 
 declare -a readme=()
 
-mapfile -t readme << "EOF"
-updoot ![status: alpha](https://img.shields.io/badge/status-alpha-red?style=flat) [![version](https://img.shields.io/github/v/tag/b0o/updoot?style=flat&color=yellow&label=version&sort=semver)](https://github.com/b0o/updoot/releases) [![license: gpl-3.0-or-later](https://img.shields.io/github/license/b0o/updoot?style=flat&color=green)](https://opensource.org/licenses/GPL-3.0)
+mapfile -t readme <<"EOF"
+updoot ![status: alpha](https://img.shields.io/badge/status-alpha-red?style=flat) [![version](https://img.shields.io/github/v/tag/b0o/updoot?style=flat&color=yellow&label=version&sort=semver)](https://github.com/b0o/updoot/releases) [![license: MIT](https://img.shields.io/github/license/b0o/updoot?style=flat&color=green)](https://opensource.org/licenses/MIT)
 ===
 
 
@@ -19,13 +19,13 @@ updoot ![status: alpha](https://img.shields.io/badge/status-alpha-red?style=flat
 EOF
 
 #shellcheck disable=2016
-mapfile -tO ${#readme[@]} readme <<< "$(
-  "${reporoot}/updoot" -H |& sed -e 's|'"$XDG_CONFIG_HOME"'|$XDG_CONFIG_HOME|g; s|'"$HOME"'|$HOME|g; s/\s*$//'
+mapfile -tO ${#readme[@]} readme <<<"$(
+  "$reporoot/updoot" -H |& sed -e 's|'"$XDG_CONFIG_HOME"'|$XDG_CONFIG_HOME|g; s|'"$HOME"'|$HOME|g; s/\s*$//'
 )"
 
-mapfile -tO ${#readme[@]} readme << "EOF"
+mapfile -tO ${#readme[@]} readme <<"EOF"
 ```
 
 EOF
 
-printf '%s\n' "${readme[@]}" > "$reporoot/README.md"
+printf '%s\n' "${readme[@]}" >"$reporoot/README.md"
